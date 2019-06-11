9e81f4198c40e6578c4f6e1e7e9fae949514fecd
David-Estevez <david.estevez.fdez@gmail.com>
2019-06-11 23:56:50 +0200

    Add function to move

--- firmware/roverto/roverto.ino
+++ firmware/roverto/roverto.ino
@@ -1,5 +1,8 @@
+//-- Roverto
+//--------------------------------------
 
 
+//-- H-bridge pinout
 static const int m1_in1 = 2;
 static const int m1_in2 = 4;
 static const int m1_pwm1 = 6;
@@ -11,9 +14,53 @@ static const int m2_pwm2 = 9;
 
 static const int enable = 5;
 
+/*
+  move - move the robot
+  v -> linear velocity
+  w -> angular velocity
+*/
+void move(int v, int w)
+{
+    //-- Compute left and right wheel speeds
+    int l_speed = constrain(v - w, -255, 255);
+    int r_speed = constrain(v + w, -255, 255);
 
+    //-- Enable motors
+    digitalWrite(enable, HIGH);
 
-void setup() {
+    //-- Set left motor
+    if (r_speed > 0)
+    {
+      digitalWrite(m1_in1, HIGH);
+      digitalWrite(m1_in2, LOW);
+      digitalWrite(m1_pwm1, LOW); //-- To be removed
+      analogWrite(m1_pwm2, 190);
+    }
+    else
+    {
+      digitalWrite(m1_in1, LOW);
+      digitalWrite(m1_in2, HIGH);
+      digitalWrite(m1_pwm1, LOW); //-- To be removed
+      analogWrite(m1_pwm2, 190);
+    }
+
+    //-- Set right motor
+    if (l_speed > 0)
+    {
+      digitalWrite(m2_in1, LOW);
+      digitalWrite(m2_in2, HIGH);
+      analogWrite(m2_pwm2, 190);
+    }
+    else
+    {
+      digitalWrite(m2_in1, HIGH);
+      digitalWrite(m2_in2, LOW);
+      analogWrite(m2_pwm2, 190);
+    }
+}
+
+void setup()
+{
   // Configure motor pins
   pinMode(m1_in1, OUTPUT);
   pinMode(m1_in2, OUTPUT);
@@ -23,33 +70,16 @@ void setup() {
   pinMode(m2_in1, OUTPUT);
   pinMode(m2_in2, OUTPUT);
   pinMode(m2_pwm2, OUTPUT);
-  
-  pinMode(enable, OUTPUT);
 
+  pinMode(enable, OUTPUT);
+  digitalWrite(enable, LOW);
 }
 
-void loop() {
-  digitalWrite(m1_in1, HIGH);
-  digitalWrite(m1_in2, LOW);
-  digitalWrite(m1_pwm1, LOW);
-  analogWrite(m1_pwm2, 190);
+void loop()
+{
 
-  digitalWrite(m2_in1, LOW);
-  digitalWrite(m2_in2, HIGH);
-  analogWrite(m2_pwm2, 190);
-  
-  digitalWrite(enable, LOW);
+  move(255, 0);
 
-  delay(200);
-
-  digitalWrite(m1_in1, HIGH);
-  digitalWrite(m1_in2, LOW);
-  digitalWrite(m1_pwm1, LOW);
-  analogWrite(m1_pwm2, 80);
-
-  digitalWrite(m2_in1, LOW);
-  digitalWrite(m2_in2, HIGH);
-  analogWrite(m2_pwm2, 80);
 
   while(true) {}
 
