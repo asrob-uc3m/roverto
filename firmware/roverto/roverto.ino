643928790eef3b62b8b0d8c536ec33e58462a519
David-Estevez <david.estevez.fdez@gmail.com>
2019-06-11 19:57:50 +0200

    Play with startup

--- firmware/roverto/roverto.ino
+++ firmware/roverto/roverto.ino
@@ -12,6 +12,7 @@ static const int m2_pwm2 = 9;
 static const int enable = 5;
 
 
+
 void setup() {
   // Configure motor pins
   pinMode(m1_in1, OUTPUT);
@@ -38,6 +39,17 @@ void loop() {
   analogWrite(m2_pwm2, 190);
   
   digitalWrite(enable, LOW);
+
+  delay(200);
+
+  digitalWrite(m1_in1, HIGH);
+  digitalWrite(m1_in2, LOW);
+  digitalWrite(m1_pwm1, LOW);
+  analogWrite(m1_pwm2, 80);
+
+  digitalWrite(m2_in1, LOW);
+  digitalWrite(m2_in2, HIGH);
+  analogWrite(m2_pwm2, 80);
 
   while(true) {}
 
